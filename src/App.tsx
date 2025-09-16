5e0d015feb8dc7f088960a4b50eb49ed9f5be109
vrohan999 <rohanprathapreddy999@gmail.com>
2025-09-16 12:27:07 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AlertsPage from "./pages/AlertsPage";
+import AlertDetailsPage from "./pages/AlertDetailsPage";
+import NasaApiIntegrationPage from "./pages/NasaApiIntegrationPage";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<AlertsPage />} />
+          <Route path="/alert/:id" element={<AlertDetailsPage />} />
+          <Route path="/nasa-api-integration" element={<NasaApiIntegrationPage />} />
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
